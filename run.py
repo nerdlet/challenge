63053f4ac9a7c1ed6264ebabece5f76898f43740
nerdlet <brndnyokabi@gmail.com>
2017-09-19 20:09:46 +0300

    add changes

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' methods=['GET', 'POST'])
 def hello_world():
     return 'Hello World!'
-    
+
 if (__name__ == "__main__"):
-	app.run(port = 5000)
+	app.run()
