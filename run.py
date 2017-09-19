addfdaf7fb12cab3bb2a2ab601286fbb77105387
nerdlet <brndnyokabi@gmail.com>
2017-09-19 21:15:21 +0300

    add files

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/' methods=['GET', 'POST'])
-def hello_world():
+@app.route('/')
+def index():
     return 'Hello World!'
 
 if (__name__ == "__main__"):
