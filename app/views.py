3fcb2997b86196a72a1b5b9a8537ef4684684dcc
nerdlet <brndnyokabi@gmail.com>
2017-09-20 12:14:27 +0300

    update files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template("getstarted.html")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/dashboard/savelistname')
+def savelistname():
+    return render_template('savelistname.html')
+
+@app.route('/dashboard/savelistname/additems')
+def additems():
+    return render_template('additems.html')
