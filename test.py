4f98cd97414fcd09cf265b327ab868f6f901b7b2
nerdlet <brndnyokabi@gmail.com>
2017-09-20 10:59:08 +0300

    update files

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-import flask
-import unitest
 
+import os
+import unittest
+
+
+ 
+class loginRegistrationTests(unittest.TestCase):
+ 
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+ 
+    # executed after each test
+    def tearDown(self):
+        pass
+ 
+    def test_index_page(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+ 
+ 
+if __name__ == "__main__":
+    unittest.main()
